3dd9318a947a81a0953cc6bbd8f9abd04e5f4f59
David Mgbede <davidmgbede75@gmail.com>
2023-11-09 14:28:44 +0100

    updates

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,22 @@
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
 $(function () {
     $('input[name="daterange"]').daterangepicker({
         opens: 'left'
@@ -5,3 +24,93 @@ $(function () {
         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
 });
+
+const swiper1 = new Swiper('.swiperFirst', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: '2000',
+
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
+    loop: true,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 900px
+        900: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: '1000',
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    loop: true,
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 900px
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        }
+    }
+});
+
+
+let addBtn = document.querySelector('.addbtn');
+let minusBtn = document.querySelector('.minusbtn');
+let countNum = document.querySelector('.count');
+let defaultCount = 0;
+
+addBtn.addEventListener('click', () => {
+    defaultCount++;
+    countNum.textContent = defaultCount;
+});
+
+minusBtn.addEventListener('click', () => {
+    if(defaultCount > 0) {
+        defaultCount--;
+        countNum.textContent = defaultCount;
+    }
+})
