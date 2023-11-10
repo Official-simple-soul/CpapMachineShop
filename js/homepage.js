810af57373d746a0491518c988060b5375a415fb
David Mgbede <davidmgbede75@gmail.com>
2023-11-10 17:37:46 +0100

    updated footer

--- js/homepage.js
+++ js/homepage.js
@@ -1,10 +1,7 @@
+//validation
 (() => {
     'use strict'
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
             if (!form.checkValidity()) {
@@ -16,6 +13,7 @@
         }, false)
     })
 })()
+//end validation
 
 $(function () {
     $('input[name="daterange"]').daterangepicker({
